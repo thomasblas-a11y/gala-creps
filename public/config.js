c627202a49586ad90f79208c3ab5ccd031f6dffa
thomasblas-a11y <thomasblas@orange.fr>
2026-05-06 16:47:32 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.GALA_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzhb3f85b0R_OP9u-bCjQVoAYe0Qm1ffv6tIw0CN8tgGYEcZOviQOxjf2wjo8V_Fd4/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbygCF2BAwRaS6qlNithgtByUaNpZvsvOWAPyFbuk3JkOmlqTnuadK11Tzm0iNMcIM2W/exec",
   sheetUrl: "https://docs.google.com/spreadsheets/d/1gk5KsezB7372UDFT_EPJf_fGeagiu4_fDWmHjK2jJzo/edit?gid=0#gid=0"
 };
